f3717914abce0b0fcd87c874262671b8a8571f1d
ShariqAli-Dev <shariqali.dev@gmail.com>
2021-11-11 21:12:34 -0500

    added notes from meeting

--- api/server.js
+++ api/server.js
@@ -1,38 +1,46 @@
-const express = require('express')
-const helmet = require('helmet')
-const cors = require('cors')
-const db = require('./data/db-config')
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+const db = require('./data/db-config');
 
-const restrict = require('./middleware/restricted.js')
+const restrict = require('./middleware/restricted.js');
 
-const clientRouter = require('./clients/clients-router.js')
-const instructorRouter = require('./instructors/instructors-router.js')
+const clientRouter = require('./clients/clients-router.js');
+const instructorRouter = require('./instructors/instructors-router.js');
 
-function getAllUsers() { return db('users') }
+function getAllUsers() {
+  return db('users');
+}
 
 async function insertUser(user) {
   // WITH POSTGRES WE CAN PASS A "RETURNING ARRAY" AS 2ND ARGUMENT TO knex.insert/update
   // AND OBTAIN WHATEVER COLUMNS WE NEED FROM THE NEWLY CREATED/UPDATED RECORD
   // UNLIKE SQLITE WHICH FORCES US DO DO A 2ND DB CALL
-  const [newUserObject] = await db('users').insert(user, ['user_id', 'username', 'password'])
-  return newUserObject // { user_id: 7, username: 'foo', password: 'xxxxxxx' }
+  const [newUserObject] = await db('users').insert(user, [
+    'user_id',
+    'username',
+    'password',
+  ]);
+  return newUserObject; // { user_id: 7, username: 'foo', password: 'xxxxxxx' }
 }
 
-const server = express()
-server.use(express.json())
-server.use(helmet())
-server.use(cors())
+const server = express();
+server.use(express.json());
+server.use(helmet());
+server.use(cors());
 
+server.use('/api/client', restrict, clientRouter);
+server.use('/api/instructor', restrict, instructorRouter);
+// server.use('/api/auth', authRouter) --> creating token, logging out etc. creaint token, buildToken()
 
-server.use('/api/client', restrict, clientRouter)
-server.use('/api/instructor', restrict, instructorRouter)
+// how we will route grabbing classses --> KNOW THERE IS A QUESTION BUT IDK HOW TO ASK IT UNTIL I START MODELING
 
 server.get('/api/users', async (req, res) => {
-  res.json(await getAllUsers())
-})
+  res.json(await getAllUsers());
+});
 
 server.post('/api/users', async (req, res) => {
-  res.status(201).json(await insertUser(req.body))
-})
+  res.status(201).json(await insertUser(req.body));
+});
 
-module.exports = server
+module.exports = server;
