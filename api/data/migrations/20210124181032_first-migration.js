cb6fb9f24bc00be545215af68089f0a6e2215ac4
ShariqAli-Dev <shariqali.dev@gmail.com>
2021-11-12 19:20:09 -0500

    created users table

--- api/data/migrations/20210124181032_first-migration.js
+++ api/data/migrations/20210124181032_first-migration.js
@@ -1,13 +1,72 @@
 exports.up = async (knex) => {
-  await knex.schema
-    .createTable('users', (users) => {
-      users.increments('user_id')
-      users.string('username', 200).notNullable()
-      users.string('password', 200).notNullable()
-      users.timestamps(false, true)
-    })
-}
+  await knex.schema.createTable('users', (users) => {
+    users.increments('user_id');
+    users.string('username', 128).notNullable();
+    users.string('password', 128).notNullable();
+  });
+};
 
 exports.down = async (knex) => {
-  await knex.schema.dropTableIfExists('users')
-}
+  await knex.schema.dropTableIfExists('users');
+};
+// users.timestamps(false, true);
+
+/* 
+exports.up = async function (knex) {
+  await knex.schema
+    .createTable('projects', (tbl) => {
+      tbl.increments('project_id');
+      tbl.string('project_name').notNullable();
+      tbl.string('project_description');
+      tbl.boolean('project_completed').defaultTo(false);
+    })
+    .createTable('resources', (tbl) => {
+      tbl.increments('resource_id');
+      tbl.string('resource_name').notNullable().unique();
+      tbl.string('resource_description');
+    })
+    .createTable('tasks', (tbl) => {
+      tbl.increments('task_id');
+      tbl.string('task_description').notNullable();
+      tbl.string('task_notes');
+      tbl.boolean('task_completed').defaultTo(false);
+      tbl
+        .integer('project_id')
+        .unsigned()
+        .notNullable()
+        .references('project_id')
+        .inTable('projects')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE');
+    })
+    .createTable('project_resources', (tbl) => {
+      tbl.increments('project_resource_id');
+      tbl.string('assigned_resource');
+      tbl
+        .integer('resource_id')
+        .unsigned()
+        .notNullable()
+        .references('resource_id')
+        .inTable('resources')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE');
+      tbl
+        .integer('project_id')
+        .unsigned()
+        .notNullable()
+        .references('project_id')
+        .inTable('projects')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE');
+    });
+};
+
+exports.down = async function (knex) {
+  await knex.schema
+    .dropTableIfExists('project_resources')
+    .dropTableIfExists('tasks')
+    .dropTableIfExists('resources')
+    .dropTableIfExists('projects');
+};
+
+*/
