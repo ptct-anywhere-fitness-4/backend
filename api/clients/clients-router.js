d5c2a1be54102e3346c48bc0b55861059df0e8cd
ShariqAli-Dev <shariqali.dev@gmail.com>
2021-11-17 20:03:08 -0500

    chore: refactored err res into using next() function

--- api/clients/clients-router.js
+++ api/clients/clients-router.js
@@ -1,85 +1,81 @@
 const router = require('express').Router();
-const Clients = require('./clients-model.js')
+const Clients = require('./clients-model.js');
 
 //get all clients
-router.get("/", async (req,res)=>{
-    Clients.getClients()
-        .then((clients)=>{
-            res.status(200).json(clients)
-        })
-        .catch((err)=>{
-            res.status(500).json({message:err})
-        })
-})
+router.get('/', async (req, res) => {
+  Clients.getClients()
+    .then((clients) => {
+      res.status(200).json(clients);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
 
 //get client by id
-router.get('/:id', async (req, res, next)=>{
-    const {id} = req.params
-    Clients.getClientById(id)
-        .then((client)=>{
-            res.status(200).json(client)
-        }).catch((err)=>{
-            res.status(500).json({message:err})
-        })
-})
+router.get('/:id', async (req, res, next) => {
+  const { id } = req.params;
+  Clients.getClientById(id)
+    .then((client) => {
+      res.status(200).json(client);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
 
 //hit this user registration endpoint if user does not provide correct passcode to become an instructor
-router.post('/', async (req,res,next)=>{
-    Clients.createClient(req.body)
-    .then((client)=>{
-        res.status(201).json(client)
+router.post('/', async (req, res, next) => {
+  Clients.createClient(req.body)
+    .then((client) => {
+      res.status(201).json(client);
     })
-    .catch(next)
-})
+    .catch((err) => {
+      next(err);
+    });
+});
 
 //get all classes available
-router.get('/classes', async (req,res,next)=>{
-    Clients.getAllClasses()
-        .then((classes)=>{
-            res.status(200).json(classes)
-        }).catch((err)=>{
-            res.status(500).json({message:err})
-        })
-})
-
+router.get('/classes', async (req, res, next) => {
+  Clients.getAllClasses()
+    .then((classes) => {
+      res.status(200).json(classes);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
 
 //get class by id
-router.get('/classes/:id', async (req,res,next)=>{
-    const { id } = req.params
-    Clients.getClassById(id)
+router.get('/classes/:id', async (req, res, next) => {
+  const { id } = req.params;
+  Clients.getClassById(id)
     .then((reqClass) => {
-        res.status(200).json(reqClass);
-      })
-      .catch((err) => {
-        res.status(500).json({ message: err });
-      });
-  });
+      res.status(200).json(reqClass);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
 
-  //get class by filter
-  router.get('/classes/:filter', async(req,res,next)=>{
-      const { filter } = req.params
-      Clients.getClassBy(filter)
-      .then((reqClass) => {
-        res.status(200).json(reqClass);
-      })
-      .catch((err) => {
-        res.status(500).json({ message: err });
-      });
-  })
+//get class by filter
+router.get('/classes/:filter', async (req, res, next) => {
+  const { filter } = req.params;
+  Clients.getClassBy(filter)
+    .then((reqClass) => {
+      res.status(200).json(reqClass);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
 
-  //register for class
-  router.post('/classes/:id', async(req,res,next)=>{
+//register for class
+router.post('/classes/:id', async (req, res, next) => {});
 
-  })
+//unregister for class
+router.delete('/classes/:id', async (req, res, next) => {});
 
-  //unregister for class
-  router.delete('/classes/:id', async(req,res,next)=>{
-
-  })
-
-
-  //get list of currently registered classes
-  router.get('/:id/classes/registered', async(req,res,next)=>{
-      
-  })
-module.exports = router
+//get list of currently registered classes
+router.get('/:id/classes/registered', async (req, res, next) => {});
+module.exports = router;
